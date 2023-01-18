63af853a998fc0b6780be4377bcc16cd1c260ffa
Binit Agarwal <rockingbinitagarwal@gmail.com>
2023-01-18 17:12:03 +0530

    did identifier boilerplate code

---
+++ didverifier/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for didverifier project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'didverifier.settings')
+
+application = get_asgi_application()
